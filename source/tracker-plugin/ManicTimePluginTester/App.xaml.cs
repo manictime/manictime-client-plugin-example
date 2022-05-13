fea4fdb5d41b79482242f2359f7d44b838a2edd3
jurijr <jurij-rozanec@hotmail.com>
2022-05-13 11:07:47 +0200

    PluginTester did not build after update to latest ManicTime libraries Fixes #11

--- source/tracker-plugin/ManicTimePluginTester/App.xaml.cs
+++ source/tracker-plugin/ManicTimePluginTester/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Windows;
+using Finkit.ManicTime.Common.SystemInformation;
+using Finkit.ManicTime.Shared.AppSettings;
 using Finkit.ManicTime.Shared.Logging;
+using Finkit.ManicTime.Shared.SystemInformation;
+using Microsoft.Extensions.Logging;
 
 namespace ManicTimePluginTester
 {
@@ -12,7 +17,14 @@ namespace ManicTimePluginTester
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            ApplicationLog.FilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "log.txt");
+            SystemInfo.Current = new WindowsSystemInfo(DateTimeOffset.Now, e.Args);
+            var appSettings = new GlobalAppSettings();
+
+            appSettings.Defaults = appSettings.Defaults.Update("paths.dataDir", Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)));
+            ApplicationLog.SetLogger(new ManicTimeLoggerFactory
+            {
+                LoggerFactory = new LoggerFactory(new[] { new FileLoggerProvider(appSettings, "log") })
+            });
         }
     }
 }
